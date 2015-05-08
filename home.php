e257a430d02b3c6b9530828cb80fe2269d76c018
Josh Mobley <josh@Joshs-MacBook-Pro.local>
2015-05-08 12:57:32 -0400

    Adding map and customizations

--- home.php
+++ home.php
@@ -101,6 +101,8 @@
 	</div>
 </section>
 
+<section id="map-canvas"></section>
+
 <section id="home-leadership" class="photo">
 	<div class="wrapper">
 		<h3 class="section-head">Leadership</h3>
@@ -109,6 +111,145 @@
 		</div>
 	</div>
 </section>
+
+<script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
+    <script>
+var map;
+
+var MY_MAPTYPE_ID = 'custom_style';
+
+function initialize() {
+
+  var featureOpts = [
+    {
+      stylers: [
+        { hue: '#000000' },
+        { saturation: -100 },
+        { lightness: -10 },
+        { visibility: 'simplified' },
+        { gamma: 0.5 },
+        { weight: 0.5 },
+        { invert_lightness: true }
+
+      ]
+    },
+    {
+      elementType: 'labels',
+      stylers: [
+        { visibilty: "off" }
+      ]
+    },
+    {
+      featureType: 'water',
+      stylers: [
+        { color: '#333333' }
+      ]
+    },
+    {
+    	featureType: "road",
+    	elementType: "labels",
+    	stylers: [
+      		{ visibility: "off" }
+    	]
+    },
+    {
+    	featureType: "poi",
+    	elementType: "labels",
+    	stylers: [
+    		{ visibility: "off" }
+    	]
+    }	
+  ];
+
+  var mapOptions = {
+    zoom: 12,
+    center: new google.maps.LatLng(35.7400373, -78.7202436),
+    mapTypeControlOptions: {
+      mapTypeIds: [google.maps.MapTypeId.ROADMAP, MY_MAPTYPE_ID]
+    },
+    mapTypeId: MY_MAPTYPE_ID,
+    scrollwheel: false
+  };
+
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+
+  var styledMapOptions = {
+    name: 'Custom Style'
+  };
+
+  var customMapType = new google.maps.StyledMapType(featureOpts, styledMapOptions);
+
+  map.mapTypes.set(MY_MAPTYPE_ID, customMapType);
+
+  var image = 'wp-content/themes/point-church/images/piont-church-marker.png';
+
+  var apex = new google.maps.Marker({
+      position: new google.maps.LatLng( 35.712722, -78.840252 ),
+      map: map,
+      title: 'Apex Campus',
+      icon: image
+  });
+
+  var apexContent = '<div id="map-info">'+
+      '<h4 class="campus-name">Apex Campus<h4>'+
+      '<p class="address">1402 E Williams St, Apex, NC 27539</p>'+
+      '</div>';
+
+  var apexInfo = new google.maps.InfoWindow({
+      content: apexContent
+  });
+
+  var raleigh = new google.maps.Marker({
+      position: new google.maps.LatLng( 35.77959, -78.67595 ),
+      map: map,
+      title: 'Raleigh Campus',
+      icon: image
+  });
+
+  var raleighContent = '<div id="map-info">'+
+      '<h4 class="campus-name">Raleigh Campus<h4>'+
+      '<p class="address">2109 Avent Ferry Rd, Raleigh, NC 27606</p>'+
+      '</div>';
+
+  var raleighInfo = new google.maps.InfoWindow({
+      content: raleighContent
+  });
+
+  var cary = new google.maps.Marker({
+      position: new google.maps.LatLng( 35.76536, -78.75111 ),
+      map: map,
+      title: 'Cary Campus',
+      icon: image
+  });
+
+  var caryContent = '<div id="map-info">'+
+      '<h4 class="campus-name">Cary Campus<h4>'+
+      '<p class="address">1503 Walnut St, Cary, NC 27511</p>'+
+      '</div>';
+
+  var caryInfo = new google.maps.InfoWindow({
+      content: caryContent
+  });
+
+  google.maps.event.addListener(apex, 'click', function() {
+    apexInfo.open(map,apex);
+  });
+
+  google.maps.event.addListener(raleigh, 'click', function() {
+    raleighInfo.open(map,raleigh);
+  });
+
+  google.maps.event.addListener(cary, 'click', function() {
+    caryInfo.open(map,cary);
+  });
+
+
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+    </script>
 
 
 <?php get_footer(); ?>
